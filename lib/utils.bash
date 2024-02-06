b2c3bf3b50cf3f8df359b3d716ba469409c6a407
Yasser Saleemi <yassersaleemi@gmail.com>
2024-02-06 15:31:32 +0000

    fix: Rename variables from MISE_ to ASDF_
    So that asdf testing works

--- lib/utils.bash
+++ lib/utils.bash
@@ -67,7 +67,7 @@ install_version() {
 
 	(
 		mkdir -p "$install_path"
-		cp -r "$MISE_DOWNLOAD_PATH"/* "$install_path"
+		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
